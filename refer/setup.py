cc4efdde3ef1507995d255c513d7ee77603c170f
David Álvarez Rosa <david@alvarezrosa.com>
2020-12-01 18:24:43 +0100

    Style changes for refer code

--- refer/setup.py
+++ refer/setup.py
@@ -1,25 +1,33 @@
+"""Explanatory message.
+
+To compile and install locally run "python setup.py build_ext --inplace" To
+install library to Python site-packages run "python setup.py build_ext install"
+
 """
-This code is for making mask.so, used to visualize the segmentation of referred object.
-All "mask" related code is copied from https://github.com/pdollar/coco.git
-"""
-from distutils.core import setup
-from Cython.Build import cythonize
-from distutils.extension import Extension
+
+
+from setuptools import setup, Extension
 import numpy as np
 
+
 ext_modules = [
-            Extension(
-                'external._mask',
-                sources=['external/maskApi.c', 'external/_mask.pyx'],
-                include_dirs = [np.get_include(), 'external'],
-                extra_compile_args=['-Wno-cpp', '-Wno-unused-function', '-std=c99'],
-            )
-        ]
+    Extension(
+        "external._mask",
+        sources=["external/maskApi.c", "external/_mask.pyx"],
+        include_dirs=[np.get_include(), "external"],
+        extra_compile_args=["-Wno-cpp", "-Wno-unused-function", "-std=c99"],
+    )
+]
 
 setup(
-    name='external',
-    packages=['external'],
-    package_dir = {'external': 'external'},
-    version='2.0',
-    ext_modules=cythonize(ext_modules)
-    )
+    name="external",
+    packages=["external"],
+    package_dir={"external": "external"},
+    install_requires=[
+        "setuptools>=18.0",
+        "cython>=0.27.3",
+        "matplotlib>=2.1.0"
+    ],
+    version="2.0",
+    ext_modules=ext_modules
+)
