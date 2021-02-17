90c21e0e2a61855e213d1021fea11d98b89ceec2
David Álvarez Rosa <david@alvarezrosa.com>
2021-02-17 10:58:04 +0100

    Update website: automagically generate gallery and get user input

--- Website/js/main.js
+++ Website/js/main.js
@@ -1,21 +1,79 @@
-function testing() {
-    alert("hello world");
+// Show selected image (from gallery, url or local storage).
+function showSelectedImg(src) {
+    let imgSelected = document.getElementById("img-selected");
+    imgSelected.src = src;
+    imgSelected.style.display = "block";
+    let imgSelectedWarn = document.getElementById("img-selected-warn");
+    imgSelectedWarn.style.display = "none";
 }
 
 
-let gallery = document.getElementById("gallery");
-let galleryImgs = gallery.getElementsByTagName("img");
+// Select image from website gallery.
+function selectImg(event) {
+    let selectedImgSrc = event.target.src;
+    showSelectedImg(selectedImgSrc);
+}
 
-for (galleryImg of galleryImgs)
-    galleryImg.onclick = testing;
+
+// Add image via URL.
+function addImg() {
+    let imgUrl = document.getElementById("img-url").value;
+    console.log(imgUrl);
+    showSelectedImg(imgUrl);
+}
 
 
+// Upload image locally from computer.
+function uploadImg() {
+    let imgLocal = document.getElementById("img-local");
+    let uploadedImg = imgLocal.files[0];
+
+    const fileReader = new FileReader();
+    fileReader.addEventListener("load", function () {
+        showSelectedImg(this.result);
+    });
+    fileReader.readAsDataURL(uploadedImg);
+}
+
+
+// Enter referring expression.
+function addReferringExpression() {
+    let referringExpression = document.getElementById("referring-expression");
+    if (referringExpression.value === "")
+        return false;
+    let reSelected = document.getElementById("re-selected");
+    reSelected.textContent = referringExpression.value;
+    reSelected.style.display = "block";
+    let reSelectedWarn = document.getElementById("re-selected-warn");
+    reSelectedWarn.style.display = "none";
+
+    return false; // We do not want our form to be submitted.
+}
+
+
+// Populate website gallery with random images from MSCOCO dataset.
+function populateGallery() {
+    const gallerySize = 9;
+    let gallery = document.getElementById("gallery");
+    gallery.innerHTML = "";
+    for (let i = 0; i < gallerySize; ++i) {
+        let imgNumber = Math.round(Math.random()*(imgFileNames.length - 1));
+        let imgFileName = imgFileNames[imgNumber];
+        let imgSrc = "datasets/refcoco/images/" + imgFileName;
+        let galleryImg = document.createElement("img");
+        galleryImg.setAttribute("src", imgSrc);
+        galleryImg.onclick = selectImg;
+        gallery.appendChild(galleryImg);
+    }
+}
+
+populateGallery();
+
+
+// Start audio recording.
 function startAudio() {
     initAudio();
     let audioContainer = document.getElementById("audio");
-    audioContainer.style.display = "flex";
+    audioContainer.style.display = "block";
 }
-
-
-
-window.addEventListener("load", startAudio);
+// window.addEventListener("load", startAudio);
--- Website/upload_wav.php
+++ Website/upload_wav.php
@@ -1,6 +1,14 @@
 <?php
 $tmp_filename = $_FILES["that_random_filename_wav"]["tmp_name"];
-move_uploaded_file($tmp_filename, "uploaded_audio.wav");
 
-echo exec("python main.py");
+
+$tmp_filename = "audio.wav";    // TODO remove this
+
+
+exec("scp audio.wav dalvarez@q.vectorinstitute.ai:~/testing/ 2>&1", $output);
+var_dump($output);
+echo exec("ssh dalvarez@q.vectorinstitute.ai 'testing/hola" . $tmp_filename . "'");
+echo exec("ls");
+
+move_uploaded_file($tmp_filename, "uploaded_audio.wav");
 ?>
