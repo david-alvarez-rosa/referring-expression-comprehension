ffee077656055866cc2ba18948c0831e9fe0a78f
David Álvarez Rosa <david@alvarezrosa.com>
2021-05-14 02:00:51 +0200

    More

--- Code/silero.py
+++ Code/silero.py
@@ -14,7 +14,7 @@ device = torch.device('cpu')
 
 model, decoder, utils = torch.hub.load(repo_or_dir='snakers4/silero-models',
                                        model='silero_stt',
-                                       language='en', # also available 'de', 'es'
+                                       language='en',
                                        device=device,
                                        verbose=False)
 
